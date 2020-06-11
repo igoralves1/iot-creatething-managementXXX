b73c7c31b9599b655667107a98a1588275d68652
igor <igoralves1@hotmail.com>
2020-06-11 11:18:53 -0400

    Get token done

---
+++ fnToken.js
@@ -0,0 +1,51 @@
+'use strict';
+
+
+var mysql = require('mysql2/promise')
+
+var pool = mysql.createPool({
+    host     : process.env.rdsMySqlHost,
+    user     : process.env.rdsMySqlUsername,
+    password : process.env.rdsMySqlPassword,
+    database : process.env.rdsMySqlDb
+})
+
+
+async function GetToken(serial_number, scuuid) {
+    try {
+        const sql = `SELECT token FROM scican.online_access_tokens WHERE serial_number='${serial_number}' AND uuid='${scuuid}' AND is_active=1`
+        const sqlResult = await pool.query(sql)
+        return sqlResult
+    } catch (error) {
+        return {
+            success: false,
+            message: "Insert to Mysql Failure",
+            error: error
+        }
+    }
+}
+
+
+module.exports.fnToken = async event => {
+
+    let sn = event.queryStringParameters.serial_number
+    let scuuid = event.queryStringParameters.scuuid
+    
+    let onlineAccessCode = 0
+    let status = 1 //! 0-Success, 1-SomeError
+
+    const sqlResult = await GetToken(sn, scuuid)
+    const token = sqlResult[0]
+    if (token.length > 0) {
+        onlineAccessCode = token[0].token
+        status = 0
+    }
+    const resp = {
+        'onlineAccessCode': onlineAccessCode,
+        'status': status
+    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify(resp)
+    }
+}
