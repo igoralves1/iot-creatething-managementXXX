37cb95a006129984655a341cb72ba9af6a6876ff
igor <igoralves1@hotmail.com>
2020-07-30 13:07:46 -0400

    Cron db

--- fnCron.js
+++ fnCron.js
@@ -1,4 +1,5 @@
 'use strict'
+const mysql = require('mysql2/promise')
 const AWS = require('aws-sdk')
 var iotdata = new AWS.IotData({endpoint: process.env.MQTT_ENDPOINT})
 
@@ -15,25 +16,41 @@ const publishMqtt = (params) =>
 // cron(0/1 * * * ? *) Each 1 min
 
 module.exports.fnCron = async (event, context, callback) => {
+  try {
+    /*
+      This lambda will run every day at 00:00:00.
 
+      Logic:
+      1 - Get a list of all serial numbers in the Db
+      2 - Pass through every one and retrieve all cycle number.
+      3 - Identify all missing cycle number.
+      4 - Publish the topic to request the missing cycle number
 
-  /*
-  This lambda will run every day at 00:00:00.
+      Note: if we have the issue with RDS and MQTT broker, lets use HTTP endpoint in order to publish what we need in the broker.
+    */
+    const pool = mysql.createPool({
+        host     : process.env.rdsMySqlHost,
+        user     : process.env.rdsMySqlUsername,
+        password : process.env.rdsMySqlPassword,
+        database : process.env.rdsMySqlDb
+    })
 
-  Logic:
-  1 - Get a list of all serial numbers in the Db
-  2 - Pass through every one and retrieve all cycle number.
-  3 - Identify all missing cycle number.
-  4 - Publish the topic to request the missing cycle number
+    const sql = `SELECT xxx FROM xxx WHERE xxx='${xxx}' AND xxx='${xxx}' AND is_active=1`
+    const sqlResult = await pool.query(sql)
+    return sqlResult
 
-  Note: if we have the issue with RDS and MQTT broker, lets use HTTP endpoint in order to publish what we need in the broker.
+  // let params = {
+  //   topic: `fnLambdaCron/today`,
+  //   payload: JSON.stringify({"attr":"cron"}),
+  //   qos: '0'
+  // };
+  // await publishMqtt(params)
 
-  */
-
-    let params = {
-        topic: `fnLambdaCron/today`,
-        payload: JSON.stringify({"attr":"cron"}),
-        qos: '0'
-    };
-    await publishMqtt(params)
+    } catch (error) {
+        return {
+            success: false,
+            message: "SELECT from Mysql Failure",
+            error: error
+        }
+    }
 }
