cf0c3b99afd75d110e16d3852f48e01db7a5e577
IgorAlves <ialves@scican.com>
2020-09-29 14:32:31 -0400

    using try catch

--- fnPublishMqttNginxLog.js
+++ fnPublishMqttNginxLog.js
@@ -27,66 +27,46 @@ async function postMongo (params) {
 
 module.exports.fnPublishMqttNginxLog = async (event) => {
 
-    const jsonBody = JSON.parse(event.body)
-    const strBody = JSON.stringify(jsonBody)
+    try {
+        const jsonBody = JSON.parse(event.body)
+        const strBody = JSON.stringify(jsonBody)
 
-    let arrstrip = strBody.match(/(\d{1,3}(?:\.\d{1,3}){3})[\s-]+\[([^\]\[]+)]\s+'([A-Z]+)\s+([^\s']+)/gm);
-    
-    let myarr = arrstrip[0].split(" ");
-    let ip = myarr[0]
-    console.log("ip", ip)
-    let dtraw = myarr[3]
-    let dt = dtraw.replace(/\[/,'')
-    console.log("dt",dt )
-    let metRaw = myarr[5]
-    let method = metRaw.replace(/\'/gi,'')
-    console.log("method",method)
-    let endpoint = myarr[6]
-    console.log("endpoint",endpoint )
+        let arrstrip = strBody.match(/(\d{1,3}(?:\.\d{1,3}){3})[\s-]+\[([^\]\[]+)]\s+'([A-Z]+)\s+([^\s']+)/gm);
+        
+        let myarr = arrstrip[0].split(" ");
 
+        let ip = myarr[0]
+        let dtraw = myarr[3]
+        let dt = dtraw.replace(/\[/,'')
+        let metRaw = myarr[5]
+        let method = metRaw.replace(/\'/gi,'')
+        let endpoint = myarr[6]
 
-    // const { payload } = jsonBody
+        let info = {
+            'ip':ip,
+            'dt':dt,
+            'method':method,
+            'endpoint':endpoint
+        }
 
-    // TODO This is the JSON String to be published 
-    /*
-    {"topic": "testpub/1", "payload": "{'tes':'igor'}"}
-    But this JSON doesn't work.
+        await postMongo(info)
 
-    This is the JSON that works and should be published:
-    '{"ddd":"zzz"}',
-    */
+        var params = {
+            topic: 'fnPublishMqttNginxLog',
+            payload: JSON.stringify(info),
+            qos: '0'
+        };
 
-    //! NOTE: Use iot-pubSub-tests-NodeJs: node post.js to publish.
-    //! If we use POSTMAN the JSON string is not well configured - we will have the follow error in the MQTT broker (We cannot display the message as JSON, and are instead displaying it as UTF-8 String.)
+        await publishMqtt(params)
 
-
-
-    const info = {
-        'ip':ip,
-        'dt':dt,
-        'method':method,
-        'endpoint':endpoint
-      }
-
-    await postMongo(info)
-
-    var params = {
-        // topic: topic,
-        topic: 'fnPublishMqttNginxLog',
-        payload: JSON.stringify(info),
-        qos: '0'
-    };
-
-    console.log('param', params)
-
-    await publishMqtt(params)
-
-    const response = {
-        "MQTT_status":"Published"
-    }
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(response)
+        return {
+            statusCode: 200,
+            body: JSON.stringify({"MQTT_status":"Published"})
+        }
+    } catch (error) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify(error)
+        }
     }
 }
