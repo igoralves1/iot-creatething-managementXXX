a19c6724e230a235e3fc8957691937ac4908df57
zehua <zzheng@scican.com>
2021-02-01 08:48:45 -0500

    Uploaded first version of Request Account Assoc Email.

---
+++ fnRequestAccountAssocEmail.js
@@ -0,0 +1,426 @@
+// 'use strict'
+// const AWS = require('aws-sdk')
+// const S3 = new AWS.S3()
+// const Joi = require('@hapi/joi')
+// var iotdata = new AWS.IotData({endpoint: process.env.MQTT_ENDPOINT});
+// const MQTT_TOPIC_ENV = process.env.mqttTopicEnv
+//
+//
+//
+// const mysql = require('mysql2/promise')
+//
+// const axios = require('axios')
+//
+//
+// const pool = mysql.createPool({
+//     host     : process.env.rdsMySqlHost,
+//     user     : process.env.rdsMySqlUsername,
+//     password : process.env.rdsMySqlPassword,
+//     database : process.env.rdsMySqlDb
+// })
+//
+// async function isUserExist(user_email) {
+//     try {
+//
+//         console.log("pool ==== ", pool)
+//
+//         const sql = `SELECT count(1) AS numbers FROM users WHERE username = '${user_email}'`
+//         console.log("sql ==== ", sql)
+//
+//         const sqlResult = await pool.query(sql)
+//         const res = sqlResult[0]
+//
+//         console.log("sqlRes ==== ", sqlResult)
+//         var userexist
+//         if (res[0].numbers == 0) {
+//             userexist = false
+//         } else {
+//             userexist = true
+//         }
+//
+//         return userexist
+//
+//     } catch (error) {
+//         console.log("🚀 0.isUserExist - error:", error)
+//         console.log("🚀 0.1.isUserExist - error:", error.stack)
+//     }
+// }
+//
+//
+//
+// const publishMqtt = (params) =>
+//     new Promise((resolve, reject) =>
+//         iotdata.publish(params, (err, res) => resolve(res)))
+//
+//
+// async function SendEmail(user_email, userExist, serial_number, language){
+//     try {
+//         var body
+//         var subject
+//         var url = "https://dev4.scican.com/send-email-template"
+//         //var url = "http://3.86.253.251/send-email-template"
+//         var data
+//         const weburl = "updates.scican.com"
+//
+//         if (userExist == true) {
+//             subject = "Account Assoc Email"
+//
+//             // body = "Hello, <br><br>" +
+//             //
+//             //     "Please, click " + "<a href='https://" + weburl + "/?action=onlineaccess&email=" + user_email + "&sn=" + serial_number + "'>here</a>" + " to complete Online Access Activation.<br><br>" +
+//             //
+//             //
+//             // "Regards,<br>" +
+//             // "The <a href='https://updates.scican.com'>updates.scican.com</a> Team."
+//
+//             body = "Hello World"
+//
+//
+//             data = {
+//                 sendto: user_email,
+//                 subject: subject,
+//                 body: body
+//             }
+//
+//
+//             console.log("data ===== ", data)
+//
+//             var axiosconnect = await axios.create()
+//
+//             console.log("axiosconnect ==== ", axios)
+//
+//             let res = await axiosconnect.post(url, data)
+//             console.log("AccountAssocEmail ==== ", res)
+//             return res.data.success
+//
+//         } else {
+//             subject = "Account Assoc Email"
+//             body = "Hello No Customer, <br><br>" +
+//
+//                 "Please, click " + "<a href='https://updates.scican.com'>here</a>" + " to complete Online Access Activation.<br><br>" +
+//
+//
+//                 "Regards,<br>" +
+//                 "The <a href='https://updates.scican.com'>updates.scican.com</a> Team."
+//
+//             data = {
+//                 sendto: user_email,
+//                 subject: subject,
+//                 body: body
+//             }
+//
+//             console.log("data ===== ", data)
+//
+//
+//             var axiosconnect = await axios.create()
+//             let res = await axiosconnect.post(url, data)
+//
+//             console.log("AccountAssocEmail ==== ", res)
+//             return res.data.success
+//         }
+//
+//     }catch (error) {
+//         console.log("🚀 0.SendEmail - error:", error)
+//         console.log("🚀 0.1.SendEmail - error:", error.stack)
+//     }
+// }
+//
+// module.exports.fnRequestAccountAssocEmail = async (event) => {
+//     try {
+//
+//         console.log("🚀 1 - event:", event)
+//         const retval = event.retval
+//         const topic = event.topic
+//         const res = topic.split("/")
+//         const serialNumber = res[2]
+//
+//         const account_email = event.account_email
+//         const language = event.language_iso639
+//
+//         //const serialNumber = "12345AB5678"
+//
+//
+//         const userCheck = await isUserExist(account_email)
+//
+//         console.log("account_email === ", account_email)
+//         console.log("userCheck === ", userCheck)
+//         console.log("serial num === ", serialNumber)
+//         console.log("language === ", language)
+//
+//         var info
+//         if (userCheck == true){
+//             await SendEmail(account_email, userCheck, serialNumber, language)
+//             info = {
+//                 "result": "email_sent"
+//             }
+//         } else {
+//             await SendEmail(account_email, userCheck, serialNumber, language)
+//             info = {
+//                 "result": "existing_account_not_found"
+//             }
+//         }
+//
+//         console.log("ready to publish MQTT ????")
+//
+//         //Q/scican/1234AB5678/srv/request/account-associate-email
+//         //Q/scican/#
+//         //Q/scican/srv/1234AB5678/response/account-associate-email
+//
+//         var params = {
+//             topic: `${MQTT_TOPIC_ENV}/scican/srv/${serialNumber}/response/account-associate-email`,
+//             payload: JSON.stringify(info),
+//             qos: '0'
+//         };
+//         await publishMqtt(params)
+//
+//
+//
+//
+//         // {
+//         //     "result": "email_sent"
+//         // }
+//         // {
+//         //     "result": "existing_account_not_found"
+//         // }
+//
+//
+//         const input = {
+//             "account_email": "928064091@qq.com",
+//             "language_iso639": "en",
+//             "language_iso3166": "US"
+//         }
+//
+//         const input1 =
+//             {
+//             "account_email": "zzheng@scican.com",
+//             "language_iso639": "en",
+//             "language_iso3166": "US"
+//             }
+//
+//
+//
+//
+//
+//
+//     } catch (error) {
+//         console.log("🚀 0 - error:", error)
+//         console.log("🚀 0.1 - error:", error.stack)
+//     }
+// }
+//
+
+
+
+
+
+
+
+
+'use strict'
+const AWS = require('aws-sdk')
+const S3 = new AWS.S3()
+const Joi = require('@hapi/joi')
+var iotdata = new AWS.IotData({endpoint: process.env.MQTT_ENDPOINT});
+const MQTT_TOPIC_ENV = process.env.mqttTopicEnv
+
+
+
+const mysql = require('mysql2/promise')
+const axios = require('axios')
+
+
+const pool = mysql.createPool({
+    host     : process.env.rdsMySqlHost,
+    user     : process.env.rdsMySqlUsername,
+    password : process.env.rdsMySqlPassword,
+    database : process.env.rdsMySqlDb
+})
+
+async function isUserExist(user_email) {
+    try {
+
+        console.log("pool ==== ", pool)
+
+        const sql = `SELECT count(1) AS numbers FROM users WHERE username = '${user_email}'`
+        console.log("sql ==== ", sql)
+
+        const sqlResult = await pool.query(sql)
+        const res = sqlResult[0]
+
+        console.log("sqlRes ==== ", sqlResult)
+        var userexist
+        if (res[0].numbers == 0) {
+            userexist = false
+        } else {
+            userexist = true
+        }
+
+        return userexist
+
+    } catch (error) {
+        console.log("🚀 0.isUserExist - error:", error)
+        console.log("🚀 0.1.isUserExist - error:", error.stack)
+    }
+}
+
+
+
+const publishMqtt = (params) =>
+    new Promise((resolve, reject) =>
+        iotdata.publish(params, (err, res) => resolve(res)))
+
+
+async function SendEmail(email, userExist, serial_num, language){
+    try {
+        var body
+        var subject
+        var url = "https://dev4.scican.com/send-email-template"
+        //var url = "http://3.86.253.251/send-email-template"
+        const weburl = "updates.scican.com"
+        var data
+        if (userExist == true) {
+            subject = "Account Assoc Email"
+            // body = "Hello, <br><br>" +
+            //
+            //     "Please, click " + "<a href='https://updates.scican.com'>here</a>" + " to complete Online Access Activation.<br><br>"
+
+
+            body = "Hello, <br><br>" +
+
+                "Please, click " + "<a href='https://" + weburl + "/?action=onlineaccess&email=" + email + "&sn=" + serial_num + "'>here</a>" + " to complete Online Access Activation.<br><br>" +
+
+
+            "Regards,<br>" +
+            "The <a href='https://updates.scican.com'>updates.scican.com</a> Team."
+
+
+            data = {
+                sendto: email,
+                subject: subject,
+                body: body
+            }
+
+            console.log("body ===== ", body)
+
+            var axiosconnect = await axios.create()
+
+            console.log("axiosconnect ==== ", axiosconnect)
+            let res = await axiosconnect.post(url, data)
+            console.log("VPC Assoc Email ==== ", res)
+            return res.data.success
+
+        } else {
+            subject = "Account Assoc Email"
+            body = "Hello No Customer, <br><br>" +
+
+                "Please, click " + "<a href='https://updates.scican.com'>here</a>" + " to complete Online Access Activation.<br><br>"
+
+            data = {
+                sendto: email,
+                subject: subject,
+                body: body
+            }
+
+            var axiosconnect = await axios.create()
+            let res = await axiosconnect.post(url, data)
+
+            console.log("VPC Email ==== ", res)
+            return res.data.success
+        }
+
+    }catch (error) {
+        console.log("🚀 0.SendEmail - error:", error)
+        console.log("🚀 0.1.SendEmail - error:", error.stack)
+    }
+}
+
+module.exports.fnRequestAccountAssocEmail = async (event) => {
+    try {
+
+        console.log("🚀 1 - event:", event)
+        const retval = event.retval
+        console.log("🚀 2 - retval:", retval)
+        const topic = event.topic
+        console.log("🚀 3 - topic:", topic)
+        const res = topic.split("/")
+        console.log("🚀 4 - res:", res)
+        const serialNumber = res[2]
+        console.log("🚀 5 - serialNumber:", serialNumber)
+
+
+        // event: {
+        //     language_iso3166: 'US',
+        //         language_iso639: 'en',
+        //         account_email: '928064091@qq.com',
+        //         topic: 'Q/scican/1234AB5678/srv/request/account-password_reset_email'
+        // }
+
+        const account_email = event.account_email
+        const language = event.language_iso639
+
+        //const serialNumber = "12345AB5678"
+
+        console.log("account_email === ", account_email)
+
+        const userCheck = await isUserExist(account_email)
+
+        console.log("userCheck === ", userCheck)
+
+        var info
+        if (userCheck == true){
+            await SendEmail(account_email, userCheck, serialNumber, language)
+            info = {
+                "result": "email_sent"
+            }
+        } else {
+            await SendEmail(account_email, userCheck, serialNumber, language)
+            info = {
+                "result": "existing_account_not_found"
+            }
+        }
+
+
+        var params = {
+            topic: `${MQTT_TOPIC_ENV}/scican/srv/${serialNumber}/response/account-associate-email`,
+            payload: JSON.stringify(info),
+            qos: '0'
+        };
+        await publishMqtt(params)
+
+
+
+
+        // {
+        //     "result": "email_sent"
+        // }
+        // {
+        //     "result": "existing_account_not_found"
+        // }
+
+
+        const input = {
+            "account_email": "928064091@qq.com",
+            "language_iso639": "en",
+            "language_iso3166": "US"
+        }
+
+        const input1111 =
+            {
+            "account_email": "zzheng@scican.com",
+            "language_iso639": "en",
+            "language_iso3166": "US"
+        }
+
+        //Q/scican/1234AB5678/srv/request/account-associate-email
+        //Q/scican/#
+        //Q/scican/srv/1234AB5678/response/account-associate-email
+
+
+
+    } catch (error) {
+        console.log("🚀 0 - error:", error)
+        console.log("🚀 0.1 - error:", error.stack)
+    }
+}
+
+
