c84f2cb51b322ce4614bd8f3bfe60e70a7bbd711
Igor ALves <ialves@scican.com>
2021-04-29 11:35:05 -0400

    added some notes in the fW functions

--- src/fw_update/fnBootFwUpdateRequestStartProcess.js
+++ src/fw_update/fnBootFwUpdateRequestStartProcess.js
@@ -54,19 +54,19 @@ module.exports.fnBootFwUpdateRequestStartProcess = async event => {
     //! P/SCICANSYS/AJCPA009/CAN/CMD/boot_fw_request_start_process/PROCESS_0_vx-x-xx_1234567890
     //! D/SCICANSYS/AJCPA009/CAN/CMD/boot_fw_request_start_process/PROCESS_0_vx-x-xx_1234567890
     //! Q/SCICANSYS/AJCPA009/CAN/CMD/boot_fw_request_start_process/PROCESS_0_vx-x-xx_1234567890
-    //! Where vx-x-xx is the version of the FirmWare to witch we want to update. Must to be in the same format of the S3 bucket: 
-    //! DEV  - iot-firmware-scican-devig1 - https://s3.console.aws.amazon.com/s3/buckets/iot-firmware-scican-devig1?region=us-east-1&tab=objects 
+    //! Where vx-x-xx is the version of the FirmWare to witch we want to update. Must to be in the same format of the S3 bucket:
+    //! DEV  - iot-firmware-scican-devig1 - https://s3.console.aws.amazon.com/s3/buckets/iot-firmware-scican-devig1?region=us-east-1&tab=objects
     //! v1-8-59/
-    //! PROD - iot-firmware-scican-prod - https://s3.console.aws.amazon.com/s3/buckets/iot-firmware-scican-prod?region=us-east-1&tab=objects 
+    //! PROD - iot-firmware-scican-prod - https://s3.console.aws.amazon.com/s3/buckets/iot-firmware-scican-prod?region=us-east-1&tab=objects
     //! v1-8-60/
 
     /*
 
     This function is trigged by the following topics:
 
-P/SCICAN/AJCPA009/CAN/CMD/boot_fw_update_request_start_process/PROCESS_0_v1-8-59_1234567890
+P/SCICAN/AJCPA009/CAN/CMD/boot_fw_update_request_start_process/PROCESS_0_v1-11-323_1234567890
 {
-    "path" : "CAN/CMD/boot_fw_update_request_start_process/PROCESS_0_v1-8-59_1234567890",
+    "path" : "CAN/CMD/boot_fw_update_request_start_process/PROCESS_0_v1-11-323_1234567890",
     "data" : {
         "sn" : "AJCPA009",
         "node" : "PROCESS"
@@ -86,7 +86,7 @@ P/SCICAN/AJCPA009/CAN/CMD/boot_fw_update_request_start_process/PROCESS_0_v1-8-59
     7A621020;Bravo G4 22;AJB;2;5;1;22;230;1000;2300;60;2000;2000;1;3;0;0;2;0;0;1;0;0;1;1;0;0;1;1;1;0;1;1;0;0;0;2943437717
     7A622000;Bravo G4 28;AJC;2;5;1;28;230;1000;2300;50;2000;2300;1;3;0;0;2;0;0;1;0;0;1;1;0;0;1;1;1;0;1;1;0;0;0;115575373
     7A622020;Bravo G4 28;AJC;2;5;1;28;230;1000;2300;60;2000;2300;1;3;0;0;2;0;0;1;0;0;1;1;0;0;1;1;1;0;1;1;0;0;0;272477946",
-            "quiet" : "1"    
+            "quiet" : "1"
         }
     }
 
@@ -134,13 +134,13 @@ P/SCICAN/AJCPA009/CAN/CMD/boot_fw_update_request_start_process/PROCESS_0_v1-8-59
             "data" : {
                 "node" : "PROCESS",
                 "config":`ref_table=${refString}`,
-                "quiet" : "1"    
+                "quiet" : "1"
             }
         }
         console.log("🚀 14 - bodyJson:", bodyJson)
         let params = {
             topic: `${MQTT_TOPIC_ENV}/MSSTER/${serialNumber}/${path}`,
-            payload: JSON.stringify(bodyJson), 
+            payload: JSON.stringify(bodyJson),
             qos: '0'
         };
         console.log("🚀 15 - params:", params)
